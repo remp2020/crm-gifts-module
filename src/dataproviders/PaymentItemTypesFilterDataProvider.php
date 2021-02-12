bf99fe4737d9bcfa08920c362fd34783218f4b5d
Matus Kalafut <kalafut.matus@gmail.com>
2021-02-12 08:36:55 +0000

    Add `paymentItemTypes` filter field in `Dashboard` presenter
    - This filter field filters payments with selected payment item types
    
    remp/crm#1709

---
+++ src/dataproviders/PaymentItemTypesFilterDataProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Crm\GiftsModule\DataProvider;
+
+use Crm\ApplicationModule\DataProvider\DataProviderException;
+use Crm\GiftsModule\PaymentItem\GiftPaymentItem;
+use Crm\PaymentsModule\DataProvider\PaymentItemTypesFilterDataProviderInterface;
+use Nette\Localization\ITranslator;
+
+class PaymentItemTypesFilterDataProvider implements PaymentItemTypesFilterDataProviderInterface
+{
+    private $filterKey = GiftPaymentItem::TYPE;
+
+    private $translator;
+
+    public function __construct(
+        ITranslator $translator
+    ) {
+        $this->translator = $translator;
+    }
+
+    public function provide(array $params)
+    {
+        if (!isset($params['paymentItemTypes'])) {
+            throw new DataProviderException('missing [paymentItemTypes] within data provider params');
+        }
+        if (!is_array($params['paymentItemTypes'])) {
+            throw new DataProviderException('invalid type of provided form: ' . get_class($params['paymentItemTypes']));
+        }
+
+        if (!isset($params['paymentItemTypesDefaultFilter'])) {
+            throw new DataProviderException('missing [paymentItemTypesDefaultFilter] within data provider params');
+        }
+        if (!is_array($params['paymentItemTypesDefaultFilter'])) {
+            throw new DataProviderException('invalid type of provided form: ' . get_class($params['paymentItemTypesDefaultFilter']));
+        }
+
+        $params['paymentItemTypes'][$this->filterKey] = $this->translator->translate("gifts.data_provider.payment_item_types_filter.key.{$this->filterKey}");
+        $params['paymentItemTypesDefaultFilter'][] = $this->filterKey;
+
+        return $params;
+    }
+
+    public function filter($selectedTypes)
+    {
+        if (in_array($this->filterKey, $selectedTypes)) {
+            return "payment_items.type = '{$this->filterKey}'";
+        }
+
+        return null;
+    }
+}
