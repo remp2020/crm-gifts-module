07fee6ce99401fe093f60103684df3b42c3673dc
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-12-20 19:54:55 +0100

    Add option for admins to edit unactivated gift subscriptions
    Editing is allowed before the email is sent to target user (so before
    payment_gift_coupon.starts_at).
    
    Added info about payment's PAID status required for gift subscription
    to be activated. (remp/helpdesk#984)
    
    Submitting is not done with help of AJAX. Running into issue with
    snippet / snippet area being not high enough. Looks like refactoring of
    gift coupon modal is needed (or not). Pushing it in current state to ease
    Christmas gift fixing after incorrect date or email were provided
    by customers.
    
    remp/crm#2096

--- src/components/GiftCoupons/GiftCoupons.php
+++ src/components/GiftCoupons/GiftCoupons.php
@@ -2,10 +2,18 @@
 
 namespace Crm\GiftsModule\Components;
 
+use Crm\ApplicationModule\Helpers\UserDateHelper;
 use Crm\ApplicationModule\Widget\BaseWidget;
 use Crm\ApplicationModule\Widget\WidgetManager;
+use Crm\GiftsModule\Repository\PaymentGiftCouponsRepository;
 use Crm\UsersModule\Repository\UsersRepository;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Multiplier;
 use Nette\Database\IRow;
+use Nette\Localization\ITranslator;
+use Nette\Utils\DateTime;
+use Tomaj\Form\Renderer\BootstrapRenderer;
+use Tracy\Debugger;
 
 /**
  * This component renders button with dropdown listing of gifted subscriptions in listing actions column.
@@ -17,15 +25,27 @@ class GiftCoupons extends BaseWidget
 {
     private $templateName = 'gift_coupons.latte';
 
+    private $paymentGiftCouponsRepository;
+
     private $usersRepository;
+
+    private $translator;
+
+    private $userDateHelper;
 
     public function __construct(
         WidgetManager $widgetManager,
-        UsersRepository $usersRepository
+        PaymentGiftCouponsRepository $paymentGiftCouponsRepository,
+        UsersRepository $usersRepository,
+        ITranslator $translator,
+        UserDateHelper $userDateHelper
     ) {
         parent::__construct($widgetManager);
 
+        $this->paymentGiftCouponsRepository = $paymentGiftCouponsRepository;
         $this->usersRepository = $usersRepository;
+        $this->translator = $translator;
+        $this->userDateHelper = $userDateHelper;
     }
 
     public function header($id = '')
@@ -61,5 +81,102 @@ class GiftCoupons extends BaseWidget
         $this->template->giftCoupons = $giftCoupons;
         $this->template->setFile(__DIR__ . '/' . $this->templateName);
         $this->template->render();
+    }
+
+    protected function createComponentGiftEditForm()
+    {
+        return new Multiplier(function ($paymentGiftCouponId) {
+
+            $paymentGiftCoupon = $this->paymentGiftCouponsRepository->find($paymentGiftCouponId);
+            if (!$paymentGiftCoupon) {
+                throw new \Exception("Unable to find payment gift coupon with ID [{$paymentGiftCouponId}].");
+            }
+
+            $formDisabled = false;
+            // disable editing if gift was already sent or start time is in less than 5 minutes (just to be sure)
+            if ($paymentGiftCoupon->status === PaymentGiftCouponsRepository::STATUS_SENT
+                || $paymentGiftCoupon->starts_at < new DateTime('now + 5 minutes')) {
+                $formDisabled = true;
+            }
+
+            $form = new Form();
+            $form->setRenderer(new BootstrapRenderer());
+            $form->setTranslator($this->translator);
+            $form->addProtection();
+
+            $form->addText('email', 'gifts.components.gift_coupons.email')
+                ->setDisabled($formDisabled);
+
+            $form->addText('starts_at', 'gifts.components.gift_coupons.start_at')
+                ->setAttribute('placeholder', 'subscriptions.data.subscriptions.placeholder.start_time')
+                ->setRequired('subscriptions.data.subscriptions.required.start_time')
+                ->setAttribute('class', 'flatpickr')
+                ->setAttribute('flatpickr_datetime_seconds', "1")
+                ->setDisabled($formDisabled);
+
+            $form->addHidden('id');
+            $form->setDefaults($paymentGiftCoupon);
+
+            $form->addSubmit('send', 'system.save')
+                ->setDisabled($formDisabled)
+                ->getControlPrototype()
+                ->setName('button')
+                ->setHtml('<i class="fa fa-save"></i> ' . $this->translator->translate('system.save'));
+
+            $form->onSuccess[] = [$this, 'formSucceeded'];
+
+            return $form;
+        });
+    }
+
+    public function formSucceeded(Form $form, array $values)
+    {
+        $paymentGiftCoupon = $this->paymentGiftCouponsRepository->find($values['id']);
+        if (!$paymentGiftCoupon) {
+            $msg = $this->translator->translate(
+                'gifts.components.gift_coupons.errors.payment_gift_coupon_not_found',
+                ['paymentGiftCouponId' => $values['id']]
+            );
+            Debugger::log("Form submitted for payment gift coupon ID {$values['id']} we are unable to find.", Debugger::ERROR);
+            $form->addError($msg);
+            // TODO: error message duplicated until AJAX submitting is implemented (modal is closed after submit; probably needs bigger refactor)
+            $this->getPresenter()->flashMessage($msg, 'error');
+            return;
+        }
+
+        if ($paymentGiftCoupon->status === PaymentGiftCouponsRepository::STATUS_SENT
+            || $paymentGiftCoupon->starts_at < new DateTime()) {
+            $msg = $this->translator->translate(
+                'gifts.components.gift_coupons.errors.already_sent',
+                ['paymentGiftCouponId' => $values['id'], 'email' => $paymentGiftCoupon->email]
+            );
+            $form->addError($msg);
+            // TODO: error message duplicated until AJAX submitting is implemented (modal is closed after submit; probably needs bigger refactor)
+            $this->getPresenter()->flashMessage($msg, 'error');
+            return;
+        }
+
+        // fix flatpickr's incorrect (missing) time zone (https://github.com/flatpickr/flatpickr/issues/1532)
+        $startsAt = DateTime::from($values['starts_at'])->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+        if ($startsAt < new DateTime()) {
+            $msg = $this->translator->translate(
+                'gifts.components.gift_coupons.errors.starts_at_in_past',
+                ['paymentGiftCouponId' => $values['id'], 'paymentGiftCouponStartsAt' => $this->userDateHelper->process($startsAt)]
+            );
+            $form->addError($msg);
+            // TODO: error message duplicated until AJAX submitting is implemented (modal is closed after submit; probably needs bigger refactor)
+            $this->getPresenter()->flashMessage($msg, 'error');
+            return;
+        }
+
+        $this->paymentGiftCouponsRepository->update($paymentGiftCoupon, [
+            'email' => $values['email'],
+            'starts_at' => DateTime::from($values['starts_at']),
+        ]);
+
+        $this->getPresenter()->flashMessage($this->translator->translate(
+            'gifts.components.gift_coupons.success',
+            ['paymentGiftCouponId' => $values['id']]
+        ));
     }
 }
