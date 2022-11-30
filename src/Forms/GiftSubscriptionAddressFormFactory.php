c1e68a23bbec5d3971985b3362b087f60fa8db6a
Miroslav Svitok <mirosvitok@gmail.com>
2022-11-30 13:21:50 +0100

    Fix overriding existing gift address for payment
    remp/helpdesk#1632

--- src/Forms/GiftSubscriptionAddressFormFactory.php
+++ src/Forms/GiftSubscriptionAddressFormFactory.php
@@ -3,64 +3,30 @@
 namespace Crm\GiftsModule\Forms;
 
 use Contributte\Translation\Translator;
-use Crm\ApplicationModule\DataProvider\DataProviderManager;
 use Crm\GiftsModule\Seeders\AddressTypesSeeder;
 use Crm\PaymentsModule\Repository\PaymentMetaRepository;
-use Crm\PaymentsModule\Repository\PaymentsRepository;
-use Crm\SubscriptionsModule\Repository\SubscriptionsRepository;
 use Crm\UsersModule\Repository\AddressChangeRequestsRepository;
 use Crm\UsersModule\Repository\AddressesRepository;
 use Crm\UsersModule\Repository\CountriesRepository;
-use Crm\UsersModule\Repository\UsersRepository;
 use Nette\Application\UI\Form;
 use Nette\Database\Table\ActiveRow;
 use Tomaj\Form\Renderer\BootstrapRenderer;
 
 class GiftSubscriptionAddressFormFactory
 {
-    const PAYMENT_META_KEY = 'gift_subscription_address';
+    public const PAYMENT_META_KEY = 'gift_subscription_address';
 
-    private $usersRepository;
-    private $addressesRepository;
-    private $countriesRepository;
-    private $addressChangeRequestsRepository;
-
-    private $dataProviderManager;
-
-    /* callback function */
     public $onSave;
 
-    /** @var ActiveRow */
     private $payment;
 
-    private $translator;
-
-    private $subscriptionsRepository;
-
-    private $paymentsRepository;
-
-    private $paymentMetaRepository;
-
     public function __construct(
-        Translator $translator,
-        UsersRepository $usersRepository,
-        AddressesRepository $addressesRepository,
-        AddressChangeRequestsRepository $addressChangeRequestsRepository,
-        CountriesRepository $countriesRepository,
-        DataProviderManager $dataProviderManager,
-        SubscriptionsRepository $subscriptionsRepository,
-        PaymentsRepository $paymentsRepository,
-        PaymentMetaRepository $paymentMetaRepository
+        private Translator $translator,
+        private AddressesRepository $addressesRepository,
+        private AddressChangeRequestsRepository $addressChangeRequestsRepository,
+        private CountriesRepository $countriesRepository,
+        private PaymentMetaRepository $paymentMetaRepository
     ) {
-        $this->translator = $translator;
-        $this->usersRepository = $usersRepository;
-        $this->addressesRepository = $addressesRepository;
-        $this->addressChangeRequestsRepository = $addressChangeRequestsRepository;
-        $this->countriesRepository = $countriesRepository;
-        $this->dataProviderManager = $dataProviderManager;
-        $this->subscriptionsRepository = $subscriptionsRepository;
-        $this->paymentsRepository = $paymentsRepository;
-        $this->paymentMetaRepository = $paymentMetaRepository;
     }
 
     public function create(ActiveRow $payment): Form
@@ -68,7 +34,15 @@ class GiftSubscriptionAddressFormFactory
         $form = new Form;
 
         $this->payment = $payment;
-        $user = $this->payment->user;
+
+        $defaults = [];
+        $oldAddress = $this->getExistingAddress($this->payment);
+        if ($oldAddress) {
+            $defaults = $oldAddress->toArray();
+            if (!$defaults['country_id']) {
+                $defaults['country_id'] = $this->countriesRepository->defaultCountry()->id;
+            }
+        }
 
         $form->addProtection();
         $form->setTranslator($this->translator);
@@ -101,9 +75,26 @@ class GiftSubscriptionAddressFormFactory
             ->setAttribute('class', 'btn btn-success')
             ->setAttribute('style', 'float: right');
 
+        $form->setDefaults($defaults);
+
         $form->onSuccess[] = [$this, 'formSucceeded'];
 
         return $form;
+    }
+
+    private function getExistingAddress(ActiveRow $payment): ?ActiveRow
+    {
+        $oldAddressId = $this->paymentMetaRepository
+            ->findByPaymentAndKey($payment, self::PAYMENT_META_KEY)
+            ?->value;
+
+        if ($oldAddressId) {
+            $address = $this->addressesRepository->find($oldAddressId);
+            if ($address && $address->type === AddressTypesSeeder::GIFT_SUBSCRIPTION_ADDRESS_TYPE) {
+                return $address;
+            }
+        }
+        return null;
     }
 
     public function formSucceeded($form, $values)
@@ -111,19 +102,11 @@ class GiftSubscriptionAddressFormFactory
         $user = $this->payment->user;
 
         if (isset($values->first_name)) {
-            // rewrite old address only if it's linked to this payment & type is correct
-            $oldAddress = false;
-            $addressID = $this->paymentMetaRepository->findByPaymentAndKey($this->payment, self::PAYMENT_META_KEY);
-            if ($addressID) {
-                $address = $this->addressesRepository->find($addressID);
-                if ($address && $address->type === AddressTypesSeeder::GIFT_SUBSCRIPTION_ADDRESS_TYPE) {
-                    $oldAddress = $address;
-                }
-            }
+            $oldAddress = $this->getExistingAddress($this->payment);
 
             $changeRequest = $this->addressChangeRequestsRepository->add(
                 $user,
-                $oldAddress,
+                $oldAddress, // rewrite old address only if it's linked to this payment and type is correct
                 $values->first_name,
                 $values->last_name,
                 null,
@@ -143,7 +126,7 @@ class GiftSubscriptionAddressFormFactory
                 $newAddress = $this->addressChangeRequestsRepository->acceptRequest($changeRequest);
 
                 // link to payment for later use (switching user from donor to donee)
-                $this->paymentMetaRepository->add($this->payment, self::PAYMENT_META_KEY, $newAddress->id, true);
+                $this->paymentMetaRepository->add($this->payment, self::PAYMENT_META_KEY, $newAddress->id);
             }
         }
 
