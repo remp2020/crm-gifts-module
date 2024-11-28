091e992d62bc7219df7ae6e98cda66a6b03f6576
Peter Dulacka <dulacka@gmail.com>
2024-11-28 13:17:45 +0100

    Conditional DI registration of scenarios donor notification
    The scenarios notification is directly dependent on REMP mailer
    module, because it needs to display list of available emails.
    However gifts module shoudln't have direct dependency
    on REMP mailer and it would crash existing installations.
    
    This commit moves the DI registration of generic event,
    and its registration to the scenario generics manager
    to GiftsModuleExtension, which executes it only when class
    from REMP Mailer module is actually available.

--- src/DI/GiftsModuleExtension.php
+++ src/DI/GiftsModuleExtension.php
@@ -3,8 +3,10 @@
 namespace Crm\GiftsModule\DI;
 
 use Contributte\Translation\DI\TranslationProviderInterface;
+use Crm\GiftsModule\Scenarios\SendNotificationEmailToDonorGenericEvent;
 use Nette\Application\IPresenterFactory;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
 
 class GiftsModuleExtension extends CompilerExtension implements TranslationProviderInterface
 {
@@ -27,6 +29,27 @@ class GiftsModuleExtension extends CompilerExtension implements TranslationProvi
         // load presenters from extension to Nette
         $builder->getDefinition($builder->getByType(IPresenterFactory::class))
             ->addSetup('setMapping', [['Gifts' => 'Crm\GiftsModule\Presenters\*Presenter']]);
+
+        // If crm/remp-mailer-module is enabled and MailTemplatesRepository is available, add generic event for scenarios.
+        //
+        // This should ideally be independent of REMP Mailer and scenario module should be responsible for providing
+        // list of available mail templates from the service it supports.
+        $hasMailTemplatesRepository = $builder->getByType(\Crm\RempMailerModule\Repositories\MailTemplatesRepository::class) !== null;
+        if ($hasMailTemplatesRepository) {
+            // register generic handler to DI
+            $definition = new ServiceDefinition();
+            $definition->setFactory(SendNotificationEmailToDonorGenericEvent::class);
+            $definition->addSetup('addAllowedMailTypeCodes', ['system', 'system_optional']);
+            $builder->addDefinition(
+                name: 'sendGiftDonorNotificationEmailGenericEvent',
+                definition: $definition,
+            );
+
+            // configure scenarios generic event
+            /** @var ServiceDefinition $genericEventsManager */
+            $genericEventsManager = $builder->getDefinition('scenariosGenericEventsManager');
+            $genericEventsManager->addSetup('register', ['send_notification_email_to_gift_donor', $definition]);
+        }
     }
 
     /**
